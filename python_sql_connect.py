8df7b35d33a968f4c4ecb6de56cdcd940b3bb3d9
abhashkumar <abhashmaddi@hotgmail.com>
2018-06-17 17:03:18 +0530

    connect sqlite3 with prepeared statement

--- python_sql_connect.py
+++ python_sql_connect.py
@@ -1,14 +1,15 @@
 import sqlite3
 
 def get_name(database_file, person_id):
-	query = "SELECT personal || ' ' || family FROM Person WHERE id='" + person_id + "';"
-	connection = sqlite3.connect(database_file)
-	cursor = connection.cursor()
-	cursor.execute(query)
-	results = cursor.fetchall()
-	cursor.close()
-	
-	print("name of the person is %s"%(results[0][0]))
-		
-		
-get_name('survey.db' , 'dyer')
+    query = "SELECT personal || ' ' || family FROM Person WHERE id=?;"
+
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
+    cursor.execute(query, [person_id])
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return results[0][0]
+
+print("Full name for dyer:", get_name('survey.db', 'dyer'))

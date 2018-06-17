1d13497e9a00f3b5acb4ca704d9ee3d4e7f8fd55
abhashkumar <abhashmaddi@hotgmail.com>
2018-06-17 17:06:23 +0530

    with insert and commit

--- python_sql_connect.py
+++ python_sql_connect.py
@@ -1,5 +1,16 @@
 import sqlite3
 
+def add_name(database_file, new_person):
+    query = "INSERT INTO Person VALUES (?, ?, ?);"
+
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
+    cursor.execute(query, list(new_person))
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+
 def get_name(database_file, person_id):
     query = "SELECT personal || ' ' || family FROM Person WHERE id=?;"
 
@@ -12,4 +23,7 @@ def get_name(database_file, person_id):
 
     return results[0][0]
 
-print("Full name for dyer:", get_name('survey.db', 'dyer'))
+# Insert a new name
+add_name('survey.db', ('barrett', 'Mary', 'Barrett'))
+# Check it exists
+print("Full name for barrett:", get_name('survey.db', 'barrett'))
